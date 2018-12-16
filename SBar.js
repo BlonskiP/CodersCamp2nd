7c862032003b2b3447277ff03d12d4ce833d6e00
Piter_Z <zeglinski.p@gmail.com>
2018-12-16 23:54:25 +0100

    Search_Bar update

--- SBar.js
+++
@@ -1,9 +0,0 @@
-function active()
-{
-    var SearchBar = document.getElementById("SearchBar");
-
-    if (SearchBar.value == "Szukaj miasta..."){
-        SearchBar.value = ""
-        SearchBar.placeholder = "Szukaj miasta..."
-    }
-}
