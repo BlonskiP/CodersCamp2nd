2088a8de05731dd8481e6512e1191be6d77cccbd
blonski <piotrblonski96@gmail.com>
2018-12-17 16:12:37 +0100

    Generator of cat divs

--- src/CatGenerator.js
+++ src/CatGenerator.js
@@ -1,14 +1,29 @@
 
-export class CatGenerator{
+export default class CatGenerator{
     constructor()
     {
         this.weather="clean";
-        console.log("CatGenerator is ONLINE :D")
+        console.log("CatGenerator is ONLINE :D");
+        this.catBox = document.createElement("DIV");
+        let body =document.body;
+        body.appendChild(this.catBox);
+        this.catBox.innerHTML="catBOX";
     }
     setWeather(weather){
         this.weather=weather;
     }
+    createCats(catsQuantity, catClass)
+    {
+        for(let i=0;i<catsQuantity;i++)
+        {
+        let cat=document.createElement("DIV");
+        cat.id="cat"+i;
+        cat.className=catClass;
+        cat.innerHTML= "cat";
+        this.catBox.appendChild(cat);
 
+        }
+    }
     changeCats(){
         switch(weather){  //wea
             case "rain":{catsRain();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import CatGenerator from "./CatGenerator";
 
 let cats = new CatGenerator();
+cats.createCats(5, "cat");
