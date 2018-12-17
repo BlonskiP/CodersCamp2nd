c0dd73b09cd060e355674e055e704a0acd5d6952
blonski <piotrblonski96@gmail.com>
2018-12-17 15:48:16 +0100

    cat init

---
+++ src/CatGenerator.js
@@ -0,0 +1,22 @@
+
+export class CatGenerator{
+    constructor(weather)
+    {
+        this.weather=weather;
+        console.log("CatGenerator is ONLINE :D")
+    }
+
+    catsRain(){
+
+    }
+    
+    catsSun(){
+
+    }
+
+    catsSnow(){
+
+    }
+
+    
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+import CatGenerator from "./CatGenerator";
